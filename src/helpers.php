5295376183ff73c61d1c45206c82134083639252
Douglas Smith <douglas.smith@akqa.com>
2020-06-08 09:58:53 -0400

    Adding initial helper functino

---
+++ src/helpers.php
@@ -0,0 +1,16 @@
+<?php
+
+use Faker\Factory;
+
+if (!function_exists('faker')) {
+    /**
+     * Generate data via the Faker\Factory.
+     *
+     * @return Faker\Factory;
+     */
+    function faker($property)
+    {
+        $faker = Factory::create();
+        return $property ? $faker->{$property} : $faker;
+    }
+}
